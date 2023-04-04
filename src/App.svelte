9486b539ca5229d507e9e032ae5c5d3e65a1291f
Seokkitdo <sjd12329@gmail.com>
2023-04-05 01:09:31 +0900

    기능 : app.svelte nav -> header 로 분리

--- src/App.svelte
+++ src/App.svelte
@@ -9,12 +9,7 @@
 <!-- Router를 사용하여 라우팅 처리 -->
 <Router>
   <!-- TODO : nav header 로 빼내기 -->
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/mypage">mypage</Link>
-    <Link to="/chat-learning">chat</Link>
-    <Link to="/login">login</Link>
-  </nav>
+
   <Route path="/" component={Home} />
   <Route path="/mypage" component={Mypage} />
   <Route path="/chat-learning" component={Chat} />
---
+++ src/components/layout/Header.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { navigate, useLocation } from 'svelte-navigator'
+  import Nav from '$components/layout/nav/Index.svelte'
+  import user from '$stores/user'
+
+  const location = useLocation()
+  const handleLogIn = async (e) => {
+    const targetText = e.target.innerText
+    if (targetText === '로그인') {
+      navigate('/login')
+    } else {
+      await user.signOut()
+      navigate('/login')
+    }
+  }
+</script>
+
+{#if $location.pathname !== '/login'}
+  <header
+    class="fixed top-0 left-0 w-full bg-gray-500 text-white text-sm leading-none z-50">
+    <div class="min-w-5xl max-w-screen-xl px-5 mx-auto">
+      <div class="flex items-center justify-between">
+        <div class="h-20 flex items-center gap-18">
+          <Nav
+            navItems={[
+              ...[
+                {
+                  name: '마이페이지',
+                  pathname: '/mypage'
+                },
+                {
+                  name: '챗러닝',
+                  pathname: 'chat-learning'
+                }
+              ]
+            ]} />
+        </div>
+        <button
+          on:click={handleLogIn}
+          class="relative -mr-5 px-5 py-2 leading-normal hover:font-bold">
+          {$user?.displayName ? '로그아웃' : '로그인'}
+        </button>
+      </div>
+    </div>
+  </header>
+{/if}
