071973103ff6a89e65be498c96f9bc8b7d950387
Seokkitdo <sjd12329@gmail.com>
2023-04-18 23:57:11 +0900

    스타일 : 파입 입력 폼 추가

--- src/components/content/Answer.svelte
+++ src/components/content/Answer.svelte
@@ -8,6 +8,19 @@
 
   onMount(scrollToBottom)
   afterUpdate(scrollToBottom)
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const dropzone = document.querySelector<HTMLInputElement>('#dropzone-file')
+    const fileLabel =
+      document.querySelector<HTMLParagraphElement>('#file-label')
+
+    dropzone.addEventListener('change', (e: Event) => {
+      const file = (e.target as HTMLInputElement).files?.[0]
+      if (file) {
+        fileLabel.textContent = file.name
+      }
+    })
+  })
 </script>
 
 <div class="flex-1 overflow-auto" id="chatContainer">
@@ -20,9 +33,39 @@
     </div>
 
     <div
-      class="group w-full text-gray-800 dark:text-gray-100 border-b border-black/10">
+      class="group w-full h-full text-gray-800 dark:text-gray-100 border-b border-black/10">
       <div
-        class="text-base gap-4 md:gap-6 md:max-w-2xl lg:max-w-xl xl:max-w-3xl p-4 md:py-6 flex lg:px-0 m-auto h-[500px]">
+        class="flex flex-col text-base gap-4 md:gap-6 md:max-w-2xl lg:max-w-xl xl:max-w-3xl p-4 md:py-6 flex lg:px-0 m-auto">
+        <div class="flex items-center justify-center w-full">
+          <label
+            for="dropzone-file"
+            class="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+            <div class="flex flex-col items-center justify-center pt-2 pb-3">
+              <svg
+                aria-hidden="true"
+                class="w-10 h-10 mb-3 text-gray-400"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                ><path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" /></svg>
+              <p
+                id="file-label"
+                class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                <span class="font-semibold">Click to upload</span> or drag and drop
+              </p>
+              <p class="text-xs text-gray-500 dark:text-gray-400">
+                SVG, PNG, JPG or GIF (MAX. 800x400px)
+              </p>
+            </div>
+            <input id="dropzone-file" type="file" class="hidden" />
+          </label>
+          <div id="file-name" class="hidden" />
+        </div>
         <div class="w-full">
           <textarea
             class="w-full resize-none border rounded-lg"
